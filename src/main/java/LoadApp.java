d00fddd7261f258e8db0be54695933035134bc7d
alina-torr <alina.ysic@gmail.com>
2021-12-25 02:22:56 +0300

    Scripted auto-commit on change (2021-12-25 02:22:56) by gitwatch.sh

--- src/main/java/LoadApp.java
+++ src/main/java/LoadApp.java
@@ -84,6 +84,6 @@ public class LoadApp {
 
     public Sink<Pair, CompletionStage<Long>> createSink() {
         return Flow.<Pair<String, Integer>>create()
-                .mapConcat((pair) -> Collections.nCopies()
+                .mapConcat((pair) -> Collections.nCopies(pair.)
     }
 }

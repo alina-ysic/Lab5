b0c2f334c4e1492c661e01ecbf76e2c4fc61bee5
alina-torr <alina.ysic@gmail.com>
2021-12-25 01:50:21 +0300

    Scripted auto-commit on change (2021-12-25 01:50:21) by gitwatch.sh

--- src/main/java/CacheActor.java
+++ src/main/java/CacheActor.java
@@ -2,9 +2,13 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 public class CacheActor extends AbstractActor {
+
+
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
+                .match(String.class, msg -> sender().tell(cache.get(msg), ActorRef.noSender()))
                 .build();
     }
 }

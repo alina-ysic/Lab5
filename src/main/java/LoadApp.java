88a9fb4adb2315712920d2c73b0e25caacdb8e8f
alina-torr <alina.ysic@gmail.com>
2021-12-25 00:16:38 +0300

    Scripted auto-commit on change (2021-12-25 00:16:38) by gitwatch.sh

--- src/main/java/LoadApp.java
+++ src/main/java/LoadApp.java
@@ -32,6 +32,8 @@ public class LoadApp {
     }
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> getFlow(Http http, ActorSystem system, ActorMaterializer materializer) {
-        return Flow.of(HttpRequest.class);
+        return Flow
+                .of(HttpRequest.class)
+                .;
     }
 }

285d60244347ebb6afa44ca3e36d90b2323435aa
alina-torr <alina.ysic@gmail.com>
2021-12-25 00:49:37 +0300

    Scripted auto-commit on change (2021-12-25 00:49:37) by gitwatch.sh

--- src/main/java/LoadApp.java
+++ src/main/java/LoadApp.java
@@ -15,7 +15,7 @@ import java.util.concurrent.CompletionStage;
 
 public class LoadApp {
 
-    private static final String URL
+    private static final String URL_PARAM = "testUrl";
 
     public static void main(String[] args) throws IOException {
         System.out.println("start!");

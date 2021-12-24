78e8d662fc5e4f4699721c4573ecd451f9d90bbf
alina-torr <alina.ysic@gmail.com>
2021-12-25 00:52:21 +0300

    Scripted auto-commit on change (2021-12-25 00:52:21) by gitwatch.sh

--- src/main/java/LoadApp.java
+++ src/main/java/LoadApp.java
@@ -42,7 +42,7 @@ public class LoadApp {
                 .map((request) -> {
                     Query queue = request.getUri().query();
                     int count = Integer.parseInt(String.valueOf(queue.get(URL_PARAM)));
-                    return null;
+                    return 5;
                 });
     }
 }

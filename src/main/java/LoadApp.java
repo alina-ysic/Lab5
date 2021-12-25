58674f090e6c4a13cc79700b85c422725667afee
alina-torr <alina.ysic@gmail.com>
2021-12-25 03:20:56 +0300

    Scripted auto-commit on change (2021-12-25 03:20:56) by gitwatch.sh

--- src/main/java/LoadApp.java
+++ src/main/java/LoadApp.java
@@ -63,7 +63,7 @@ public class LoadApp {
                 })
                 .mapAsync(ASYNC_COUNT, (pair) -> Patterns.ask(cacheActor, pair.first(), TIMEOUT)
                             .thenCompose((result) -> {
-                                if (result != null) return CompletableFuture.completedFuture(result);
+                                if (result != null) return CompletableFuture.completedFuture(new Pair<>(pair.first(), (Integer) result));
                                 return ping(pair, materializer);
                             }))
                 .map((result) -> {
